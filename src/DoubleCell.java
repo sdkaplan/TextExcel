03db1e31663e5464cac822146ad83930fd4ba331
sdkaplan <sydsdk@gmail.com>
2016-01-28 08:04:13 -0800

    added new classes

---
+++ src/DoubleCell.java
@@ -0,0 +1,9 @@
+/**
+ * Created by bal_Sdkaplan on 1/27/2016.
+ */
+public class DoubleCell extends Cell {
+
+    public DoubleCell() {
+        super();
+    }
+}
---
+++ src/StringCell.java
@@ -0,0 +1,9 @@
+/**
+ * Created by bal_Sdkaplan on 1/27/2016.
+ */
+public class StringCell extends Cell {
+
+    public StringCell(){
+f
+    }
+}
